5beb68302ae7a9f22be54c009df8b306138e5cc4
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 03:52:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,8 @@ def create_project_hash
   # titles: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
+  # Location: project.css(".location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   binding.pry
 end
 
